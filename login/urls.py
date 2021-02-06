ed20bd51e6ec4099311213b0d350b4dbbcca9edf
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-06 10:06:17 -0600

    add urls for login/register

--- login/urls.py
+++ login/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
+    path('register', views.register),
+    path('user_login', views.user_login),
+    path('log_off', views.log_off),
 ]
